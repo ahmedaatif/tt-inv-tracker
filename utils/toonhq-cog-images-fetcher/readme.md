5c76abe06256ddabd41a86fc6a70f54eeefee4fe
Ahmed A'atif <ahmedaatif1999@gmail.com>
2024-06-15 16:30:06 +0300

    fetch toonhq cog images within the same directory

--- utils/toonhq-cog-images-fetcher/readme.md
+++ utils/toonhq-cog-images-fetcher/readme.md
@@ -9,6 +9,5 @@ This Bash Shell script was created to fetch all cog images quickly from the [Too
 
 - <i>Notes</i>
   1. You might need [WSL](https://ubuntu.com/desktop/wsl) to run Bash Shell scripts in Windows (Untested as of now)</i>
-  2. <b>Do NOT run this script outside the scope of this project;</b> as it is using a relative path to access the `src/assets/cogs` directory.
 
-3. Success! It will download all cog portraits under `src/assets/cogs`
+3. Success! It will download all cog portraits under this directory.
--- utils/toonhq-cog-images-fetcher/script.sh
+++ utils/toonhq-cog-images-fetcher/script.sh
@@ -11,19 +11,13 @@ fi
 
 cog_names=("flunky" "bottom_feeder" "short_change" "cold_caller" "pencil_pusher" "bloodsucker" "penny_pincher" "telemarketer" "yes_man" "double_talker" "tightwad" "name_dropper" "micromanager" "ambulance_chaser" "bean_counter" "glad_hander" "downsizer" "backstabber" "number_cruncher" "mover_and_shaker" "head_hunter" "spin_doctor" "money_bags" "two_face" "corporate_raider" "legal_eagle" "loan_shark" "the_mingler" "the_big_cheese" "big_wig" "robber_baron" "mr_hollywood")
 
-if test -d ../../src/assets/cogs; then
-  echo "Directory exists - Not creating the cogs directory under assets"
-else
-  mkdir ../../src/assets/cogs
-fi
-
 for cog_name in "${cog_names[@]}"; do
 
   full_url="$base_url$cog_name.png"
 
   echo "URL: $full_url"
 
-  $download_tool -o ${cog_name//_}.png "$full_url" --output-dir ../../src/assets/cogs
+  $download_tool -o ${cog_name//_}.png "$full_url"
 
   if [[ $? -eq 0 ]]; then
     echo "Downloaded: $cog_name"
